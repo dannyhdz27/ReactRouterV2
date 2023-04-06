f289104f80e9f57b6d9ebdad0e4d77fc3e13ce4b
Daniel H <dannyhdz27@gmail.com>
2023-04-06 10:15:14 -0500

    working react routers

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,13 +1,25 @@
 import Red from "./Red";
 import Blue from "./Blue";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const Main = (props) => {
   return (
-    <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
-    </div>
+    <BrowserRouter>
+      <div id="container">
+        <h1>Hello React Router!</h1>
+        <div id="navbar">
+          <Link to="/blue">Go to Blue</Link>
+          <Link to="/red">Go to Red</Link>
+        </div>
+        <div id="main-section">
+          <Routes>
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
+            <Route path="/" element={<Red />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Main from "./Main";
+import { BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
